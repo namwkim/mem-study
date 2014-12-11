b7ae5237de95141f121d736b2bfd026f7d3e19d1
NAM WOOK KIM <namwkim@seas.harvard.edu>
2014-12-10 19:54:35 -0500

    an initial full working pipeline

--- routes/bubble.js
+++ routes/bubble.js
@@ -1,6 +1,10 @@
 var express = require('express');
-var router = express.Router();
-var _ = require('underscore');
+var http    = require('http');
+var _       = require('underscore');
+var request = require('request');
+var qs      = require('querystring');
+//var simple_recaptcha = require('simple-recaptcha');
+var router  = express.Router();
 
 router.get('/', function(req, res) {
 	console.log(req.params);
@@ -26,6 +30,41 @@ router.get('/images', function(req, res){
             res.json({ targets: targets, blurred: blurred});
         }
 	});
+});
+router.post('/recaptcha', function(req, res){
+    
+    var postData = {
+        secret :'6Lc4B_8SAAAAALzTevL05JYiED5zHfiLaHNE2KwJ',
+        ip : req.ip,
+        response : req.body.recaptcha_response
+    };
+    var url = 'https://www.google.com/recaptcha/api/siteverify?'+qs.stringify(postData);
+    console.log(url);
+    
+    // Set up the request
+    request.post(
+        url,
+        {},
+        function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                
+                
+                body = JSON.parse(body);
+                console.log(body);
+                if (body.success==true){
+                    res.send({code: 0, message:"successfully verified!"});
+                }else{
+                    res.send({code: 1, message:"verification failed!"});
+                }
+            }
+        });
+
+/*
+    simple_recaptcha(privateKey, ip, challenge, response, function(err) {
+        if (err) res.send({code: 1, message:err.message});
+        res.send({code: 0, message:"verified!"});
+    });
+*/
 });
 router.post('/log', function(req, res){
 	var db 		= req.bubbledb;
