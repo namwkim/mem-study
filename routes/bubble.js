d66280b3deb48dc3294f4db878544912063c39b5
NAM WOOK KIM <namwkim@seas.harvard.edu>
2014-12-20 21:02:17 -0500

    debug

--- routes/bubble.js
+++ routes/bubble.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 router.get('/images', function(req, res){
 	var db = req.bubbledb;
 	var hitId = req.body.hitId;
+    console.log(hitId);
 	db.collection('images').find({hit_id: hitId}).toArray(function(err, result){
         if (err) {
             return console.log(new Date(), 'error in loading images', err);
