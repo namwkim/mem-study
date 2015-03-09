178f55b79f76dd262890a8bf919726c95f910d87
NAM WOOK KIM <namwkim@seas.harvard.edu>
2015-03-09 19:51:38 -0400

    fix

--- scripts/coding-create-HIT.py
+++ scripts/coding-create-HIT.py
@@ -5,7 +5,7 @@ import os, pymongo, sys, random, csv, copy
 
 ######  AMT CONFIGURATION PARAMETRS  ######
 
-SANDBOX = True  # Select whether to post to the sandbox (using fake money), or live MTurk site (using REAL money)
+SANDBOX = False  # Select whether to post to the sandbox (using fake money), or live MTurk site (using REAL money)
 HIT_URL = "https://study.namwkim.org/coding"  # Provide the URL that you want workers to sent sent to complete you task
 
 NUMBER_OF_HITS = 15  # Number of different HITs posted for this task
