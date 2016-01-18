638bc261e454d9e61c4371e816a9cf3af2562b24
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-01-18 13:30:38 -0500

    update

--- scripts/bubble-refine-logs.py
+++ scripts/bubble-refine-logs.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 	#client 	= pymongo.MongoClient('54.69.103.85', 27017)
 	client 	= pymongo.MongoClient('localhost', 27017)
 	db 		= client.bubblestudy	
-	fromCol = db.logs32_Dec
-	toCol 	= db.refinedLogs32_Dec
+	fromCol = db.logs40_Dec
+	toCol 	= db.refinedLogs40_Dec
 	toCol.remove({})
 
 	# filter data
