3fbd49830f3e022d48eab3cb4064ee9635a31795
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-01-29 14:59:51 -0500

    update

--- scripts/salicon/blur_images.m
+++ scripts/salicon/blur_images.m
@@ -1,21 +1,27 @@
 
 % load all images
-images = dir('./targets/*.jpg');
+% images = dir('./targets/*.jpg');
+image = 'salicon-practice';
 radi = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80];
 for i=1:length(radi)
-    blur_dir = sprintf('targets_blurred_%d', radi(i));
-    mkdir(blur_dir)
+%     blur_dir = sprintf('targets_blurred_%d', radi(i));
+%     mkdir(blur_dir)
 
     H = fspecial('gaussian',radi(i),radi(i)); 
 
     % read image
-    for j=1:length(images)
-        imgpath = sprintf('./targets/%s', images(j).name);
-        im = imread(imgpath);
-        blurim = imfilter(im,H,'replicate');
-        imgpath = sprintf('./%s/%s', blur_dir, images(j).name);
-        imwrite(blurim, imgpath);
-    end
+%     for j=1:length(images)
+%         imgpath = sprintf('./targets/%s', images(j).name);
+%         im = imread(imgpath);
+%         blurim = imfilter(im,H,'replicate');
+%         imgpath = sprintf('./%s/%s', blur_dir, images(j).name);
+%         imwrite(blurim, imgpath);
+%     end
+    imgpath = sprintf('./%s.jpg', image);
+    im = imread(imgpath);
+    blurim = imfilter(im,H,'replicate');
+    imgpath = sprintf('./%s-blurred_%d.jpg',  image, radi(i));
+    imwrite(blurim, imgpath);
 end
     
 % blur 
