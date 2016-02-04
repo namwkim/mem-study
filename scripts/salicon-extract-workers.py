d0a02b2e3ba64cbf571c3a090503f7c6b723429a
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-02-04 11:05:05 -0500

    update

--- scripts/salicon-extract-workers.py
+++ scripts/salicon-extract-workers.py
@@ -2,17 +2,16 @@
 import os, pymongo, sys, datetime, csv
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print "provide a filename to save worker ids"
         sys.exit(0)
     # open remote database
     client 	= pymongo.MongoClient('54.69.103.85', 27017)
-    db 		= client.bubblestudy
-    logs = db.logs32_Dec.find({})
-    print logs.count()
+    db 		= client.saliconstudy;
+    logs = db[sys.argv[1]].find({"action":"start-experiment"})
 
     workers = dict()
-    with open(sys.argv[1], 'wb') as f:
+    with open(sys.argv[2], 'wb') as f:
         writer = csv.writer(f)
         for log in logs:
             if workers.has_key(log["worker_id"]):
@@ -20,4 +19,4 @@ if __name__ == "__main__":
             workers[log["worker_id"]] = log["worker_id"]
             print workers[log["worker_id"]]
             writer.writerow([log["worker_id"]])
-    print "extractioin finished!"
+    print "Count:", logs.count()
