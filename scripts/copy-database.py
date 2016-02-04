a4b82f3b0946641b85b6f2ac56cf311abfd79078
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-02-04 16:09:34 -0500

    update

---
+++ scripts/copy-database.py
@@ -0,0 +1,14 @@
+import os, pymongo, sys
+
+
+
+if len(sys.argv) < 2:
+    print "provide a database name"
+    sys.exit(0)
+print "copying", sys.argv[1]
+localClient = pymongo.MongoClient('localhost', 27017)
+localClient.drop_database(sys.argv[1]);
+localClient.admin.command('copydb',
+                 fromdb=sys.argv[1],
+                 todb=sys.argv[1],
+                 fromhost='54.69.103.85:27017')
