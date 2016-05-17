1ed8f223ab7940c09b64ac69bc295dddb0aeba30
Nam Wook Kim <namwkim85@gmail.com>
2016-05-18 01:45:12 +0900

    update

---
+++ public/images/filter-all5k-images.py
@@ -0,0 +1,40 @@
+import os, pymongo, sys, random, time, csv, urllib2, shutil
+from PIL import Image
+
+# load csv
+url = 'https://raw.githubusercontent.com/massvis/dataset/master/csv_files/all5k_metadata.csv'
+res = urllib2.urlopen(url)
+cr  = csv.reader(res)
+next(cr)
+
+labelMap = {}
+for row in cr:
+    filename = os.path.splitext(row[0])[0]
+    source   = row[2]
+    # print filename, source
+    labelMap[filename] = (filename, source)
+
+# mkdir filtered
+if not os.path.exists('filtered_all5k'):
+	os.makedirs('filtered_all5k')
+print 'label size', len(labelMap)
+# filter image names
+source_dir = "./all5k";
+for root, dirs, files in os.walk(source_dir):
+	print 'len', len(files)
+	for file in files:
+		if file.startswith('.'):
+			continue
+		# print file
+		filename = os.path.splitext(file)[0]
+		# print filename
+		if labelMap.has_key(filename) and labelMap[filename][1]=="S":
+			print labelMap[filename]
+        		# move filtered images
+        		shutil.move('./all5k/'+file, './filtered_all5k/'+file)
+        	if os.path.isfile("./all5k/" + file):
+    			im=Image.open("./all5k/" + file)
+    			ratio = im.size[0]/float(im.size[1])
+    			if ratio<0.5 or ratio > 2.0:
+    				print filename, ratio, labelMap[filename][1]
+    				shutil.move('./all5k/'+file, './filtered_all5k/'+file)
