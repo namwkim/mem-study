871ba9a8964cebf8b8d465d866a7f64f298d9e9a
Nam Wook Kim <namwkim85@gmail.com>
2016-05-28 16:23:39 +0200

    update

--- public/images/resize_all5k.m
+++ public/images/resize_all5k.m
@@ -1,8 +1,8 @@
 % load all images
-loadpath = './all5k-reduced'
+loadpath = './all5k'
 all5k= dir(strcat(loadpath, '/*.png'));
 % mkdir to which resized files are copied
-savepath = './all5k';
+savepath = './temp_dir';
 if exist(savepath, 'dir')
     rmdir(savepath,'s');
 end
@@ -14,15 +14,23 @@ for i=1:length(all5k)
     image = all5k(i);
     imgpath = sprintf(strcat(loadpath, '/%s'), image.name);
     imgpath
-    im  = imread(imgpath);
+    [im, map]  = imread(imgpath);
     % resize 
     [H, W, D] = size(im);
     ratio = min(maxW/W, maxH/H);  
     if ratio>1.0
         imr = im;
     else
-        imr = imresize(im, ratio);
+        if isempty(map)
+            imr = imresize(im, ratio);
+        else
+            [imr, map] = imresize(im, map, ratio);
+        end
     end
     imgpath = sprintf(strcat(savepath, '/%s'), image.name);
-    imwrite(imr, imgpath);
+    if isempty(map)
+        imwrite(imr, imgpath);
+    else
+        imwrite(imr, map, imgpath);
+    end
 end
