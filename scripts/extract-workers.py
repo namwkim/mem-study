6353542420249481ce68431e7d650523c9249b14
Nam Wook Kim <namwkim85@gmail.com>
2017-05-29 15:06:54 -0700

    update

--- scripts/extract-workers.py
+++ scripts/extract-workers.py
@@ -1,14 +1,27 @@
 
-import os, pymongo, sys, datetime, csv
+import os
+import pymongo
+import sys
+import datetime
+import csv
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         print "provide a database, collection, and filename."
         sys.exit(0)
-    # open remote database
-    client 	= pymongo.MongoClient('54.69.103.85', 27017)
-    db 		= client[sys.argv[1]]
-    logs = db[sys.argv[2]].find({'action':'survey'})
+    # open db connection
+    dbauth = csv.reader(open('../auth.txt', 'r')).next()
+    dbauth[0] = dbauth[0].strip()
+    dbauth[1] = dbauth[1].strip()
+
+    dburl = 'mongodb://' + \
+        dbauth[0] + ':' + dbauth[1] + \
+            '@54.69.103.85:27017/?authSource=admin'
+    # print dburl
+    client = pymongo.MongoClient(dburl)
+
+    db = client[sys.argv[1]]
+    logs = db[sys.argv[2]].find({'action': 'survey'})
     print logs.count()
 
     workers = dict()
